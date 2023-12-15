8393571a917852e78e3b525efd4677cf50892bf9
Donny Satriya <ssatriya@gmail.com>
2023-12-15 15:22:10 +0800

    Refactor: Testing home sidebar bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,11 @@ import { redirect } from "next/navigation";
 import AuthWrapper from "@/components/auth-wrapper";
 
 export default async function Home() {
-  const session = await getCurrentSession();
+  // const session = await getCurrentSession();
 
-  if (session?.user) {
-    return redirect("/home");
-  }
+  // if (session?.user) {
+  //   return redirect("/home");
+  // }
 
   return (
     <div className="px-4 pb-4 md:p-4 flex flex-col h-screen justify-between">
