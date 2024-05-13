0fbb8d811d2f8c345665507f855f8dc2c5184bcb
Donny Satriya <ssatriya@gmail.com>
2024-05-13 11:34:10 +0800

    moved to the newer version with neon postgre

--- src/components/auth-form.tsx
+++ src/components/auth-form.tsx
@@ -9,19 +9,6 @@ export default function AuthForm() {
   return (
     <div className="flex h-full justify-start flex-col gap-4">
       <div className="w-[300px] flex flex-col gap-3">
-        <Link href="/api/login/google">
-          <Button className="w-full rounded-full bg-white hover:bg-zinc-200">
-            <Icons.google className="h-4 w-4 mr-2" />
-            <p className="font-bold text-black">Sign up with Google</p>
-          </Button>
-        </Link>
-
-        <Link href="/api/login/google">
-          <Button className="w-full  rounded-full bg-white hover:bg-zinc-200">
-            <Icons.apple className="h-6 w-6 mr-2" />
-            <p className="font-bold text-black">Sign up with Apple</p>
-          </Button>
-        </Link>
         <div className="flex justify-center items-center w-full">
           <div className="h-[1px] w-full rounded-full bg-slate-700" />
           <span className="px-2">or</span>
