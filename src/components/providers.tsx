730357647a5fe256da6852edcd8545d8a86e7428
ssatriya <ssatriya@gmail.com>
2024-10-01 08:41:16 +0800

    feat: add progressbar on route change

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,13 +1,22 @@
 "use client";
 
 import { PropsWithChildren } from "react";
+import { AppProgressBar } from "next-nprogress-bar";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
 const Providers = ({ children }: PropsWithChildren) => {
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <AppProgressBar
+        height="4px"
+        color="#1D9BF0"
+        options={{ showSpinner: false }}
+        shallowRouting
+      />
+      {children}
+    </QueryClientProvider>
   );
 };
 
