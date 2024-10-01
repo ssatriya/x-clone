97d33d5f59d970b56e117cc67d92632c91a491ca
ssatriya <ssatriya@gmail.com>
2024-10-01 19:32:51 +0800

    refactor: use css only button tooltip

--- src/components/button-tooltip.tsx
+++ src/components/button-tooltip.tsx
@@ -1,53 +1,24 @@
 "use client";
 
-import usePopperInstance from "@/hooks/usePopperInstance";
-import { PropsWithChildren, useEffect, useRef, useState } from "react";
+import { PropsWithChildren } from "react";
 
 type Props = {
   content: string;
 };
 
 const ButtonTooltip = ({ content, children }: PropsWithChildren<Props>) => {
-  const [tooltipVisible, setTooltipVisible] = useState(false);
-  const referenceRef = useRef<HTMLDivElement>(null);
-  const tooltipRef = useRef<HTMLDivElement>(null);
-
-  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
-
-  usePopperInstance(referenceRef, tooltipRef, tooltipVisible);
-
-  const handleTooltipVisibility = (show: boolean) => {
-    if (timeoutRef.current) clearTimeout(timeoutRef.current);
-    timeoutRef.current = setTimeout(() => setTooltipVisible(show));
-  };
-
-  useEffect(() => {
-    return () => {
-      if (timeoutRef.current) clearTimeout(timeoutRef.current);
-    };
-  }, []);
-
   return (
-    <div className="inline-block">
+    <div className="inline-block group relative w-fit">
+      {children}
       <div
-        ref={referenceRef}
-        onMouseEnter={() => handleTooltipVisibility(true)}
-        onMouseLeave={() => handleTooltipVisibility(false)}
+        className="absolute left-1/2 -translate-x-1/2 top-full
+          z-50 w-max transition-opacity duration-300 
+          cursor-default text-[11px] leading-3 text-white p-1 
+          rounded-sm bg-hover-tooltip 
+          group-hover:opacity-100 group-hover:visible 
+          opacity-0 invisible"
       >
-        {children}
-      </div>
-      <div
-        ref={tooltipRef}
-        onMouseEnter={() => {
-          if (timeoutRef.current) {
-            clearTimeout(timeoutRef.current);
-          }
-        }}
-        onMouseLeave={() => handleTooltipVisibility(false)}
-        className={`z-50 transition-opacity w-fit absolute cursor-default text-[11px] leading-3 text-white p-1 rounded-sm bg-hover-tooltip
-           ${tooltipVisible ? "opacity-100 visible" : "opacity-0 invisible"}`}
-      >
-        {content}
+        <span>{content}</span>
       </div>
     </div>
   );
